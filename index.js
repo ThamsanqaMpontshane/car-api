0c0b238ded9bd833c04535c0be5272ce50613ed6
ThamsanqaMpontshane <thamsanqalm01@gmail.com>
2022-11-14 11:27:33 +0200

    last commit

--- index.js
+++ index.js
@@ -10,24 +10,59 @@ const carsElement = document.querySelector('.myCars');
 axios.get('https://api-tutor.herokuapp.com/v1/colors')
     .then((response) => {
         response.data.forEach((color) => {
-            theColors.innerHTML += `<li>${color}</li>`;
-        });
-    }
-);
+            const colorButton = document.createElement('button');
+            colorButton.innerHTML = color;
+            colorButton.style.color = color;
+            colorButton.id = color;
+            theColors.appendChild(colorButton);
+            colorButton.addEventListener('click', () => {
+                if (colorButton.id === color) {
+                    axios.get('https://api-tutor.herokuapp.com/v1/cars/color/' + color)
+                        .then((response) => {
+                            const cars = response.data;
+                            carsElement.innerHTML = carsTemplate({cars});
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                        }
+                        )
+                }
+            })
+        })
+    })
+
 
 axios.get('https://api-tutor.herokuapp.com/v1/makes')
     .then((response) => {
         response.data.forEach((brand) => {
-            theBrands.innerHTML += `<li>${brand}</li>`;
+            const brandButton = document.createElement('button');
+            brandButton.innerHTML = brand;
+            brandButton.id = brand;
+            theBrands.appendChild(brandButton);
+            brandButton.addEventListener('click', () => {
+                if (brandButton.id === brand) {
+                    axios.get('https://api-tutor.herokuapp.com/v1/cars/make/' + brand)
+                        .then((response) => {
+                            const cars = response.data;
+                            carsElement.innerHTML = carsTemplate({cars});
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                        }
+                        )
+                }
+            }
+            )
         });
     }
 );
 
-axios.get('https://api-tutor.herokuapp.com/v1/cars')
-    .then(result => {
-        const cars = result.data;
-        carsElement.innerHTML = carsTemplate({ cars });
-    }
-);
+// axios.get('https://api-tutor.herokuapp.com/v1/cars')
+//     .then(result => {
+//         const cars = result.data;
+//         carsElement.innerHTML = carsTemplate({ cars });
+//     }
+// );
+
 
 
